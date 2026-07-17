3d44cd996ae1e09b4e37edd7c5651f02bf76391a
Saravanan <saravanansn2622007@gmail.com>
2026-07-17 15:00:18 +0530

    Add files via upload

---
+++ Name.java
@@ -0,0 +1,17 @@
+class Name{
+public static void main(String[] args){
+byte a=127;
+short b=257;
+int c=123456789;
+long d=1234567898;
+float e=1.1234567;
+double f=1.12345687912;
+System.out.println(a);
+System.out.println(b);
+System.out.println(c);
+System.out.println(d);
+System.out.println(e);
+System.out.println(f);
+System.out.println("JEEVA SIR IS THE BEST TEACHER");
+}
+}
---
+++ New.java
@@ -0,0 +1,16 @@
+class New{
+public static void main(String[] args){
+byte a=127;
+short b=257;
+int c=123789;
+long d=1234567;
+float e=1.127f;
+double u=1.12345;
+System.out.println(a b"  ");
+System.out.println(c);
+System.out.println(d);
+System.out.println(e);
+System.out.println(u);
+System.out.println("JEEVA SIR IS THE BEST TEACHER");
+}
+}
